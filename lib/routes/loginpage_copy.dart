eec159689f954a25b173a9d180e133e176d6025d
Liang Ruiyang <2123358697@qq.com>
2021-12-12 14:27:33 +0800

    add: 看了MOOC的视频，好像明白了什么，关键在214行
    不写了不写了，明白了大概的思路，关键在errorText为空，有时间再写

--- lib/routes/loginpage_copy.dart
+++ lib/routes/loginpage_copy.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:zhihu_news/tabs/home_content.dart';
@@ -86,7 +87,7 @@ class _TestPageState extends State<HomePage> {
           buildImage(),
           //用户名输入框
           buildUserNameWidget(),
-          const SizedBox(height: 20,),
+          //const SizedBox(height: 20,),
           //用户密码输入框
           buildUserPasswordWidget(),
           const SizedBox(height: 40,),
@@ -186,13 +187,9 @@ class _TestPageState extends State<HomePage> {
   ///用户名输入框 Stream 局部更新 error提示
   ///     ShakeAnimationWidget 抖动动画
   ///
-  StreamBuilder<String> buildUserNameWidget() {
-    return StreamBuilder<String>(
-      stream: _userNameStream.stream,
-      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-        return
-          Padding(
-            padding: const EdgeInsets.all(8.0),
+  Widget buildUserNameWidget() {
+    return Padding(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
             child: TextField(
               //焦点控制
               focusNode: _userNameFocusNode,
@@ -208,41 +205,38 @@ class _TestPageState extends State<HomePage> {
                   FocusScope.of(context).requestFocus(_userNameFocusNode);
                 }
               },
-              // onChanged: (String value){
-              //   if(checkUserName()){
-              //     //_userNameController.clear();
-              //   }
-              // },
-              // onTap: (){
-              //   if(checkUserName()){
-              //     _userNameStream.close();
-              //   }
-              // },
-              //边框样式设置
-              decoration: InputDecoration(
-                //红色的错误提示文本
-                errorText: snapshot.data,
 
+              //边框样式设置
+              decoration: const InputDecoration(
+                //红色的错误提示文本
                 labelText: "用户名",
+
+                errorText: null,
+
+                // counterText: "nihao",
+                // counterStyle: TextStyle(
+                //   color: Colors.red,
+                // ),
+                // counter:Text(
+                //   "nihao",
+                //   textAlign: TextAlign.left,
+                // ),
                 //设置上下左右 都有边框
                 //设置四个角的弧度
-                focusedBorder: const OutlineInputBorder(
+                focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(color:Colors.blue)
                 ),
                 // enabledBorder: const OutlineInputBorder(
                 //     borderSide: BorderSide(color:Colors.blue)
                 // ),
-                border: const OutlineInputBorder(
+                border: OutlineInputBorder(
                   //设置边框四个角的弧度
                   borderSide: BorderSide(color:Colors.blue),
                   borderRadius: BorderRadius.all(Radius.circular(10)),
-
                 ),
               ),
             ),
           );
-      },
-    );
   }
 
   void checkLoginFunction() {
